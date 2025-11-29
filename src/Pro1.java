9c152ba1e0196e647d531352fd9b0bb7b0b97d11
Gourav Dodia <gouravkumardodia@gmail.com>
2025-11-29 13:14:32 +0530

    changes

--- src/Pro1.java
+++ src/Pro1.java
@@ -8,10 +8,14 @@ import model.Student;
 
 public class Pro1 {
 
-	public static void main(String[] args) {
-
-		Configuration cfg = new Configuration();
-		cfg.configure();
+	
+		SessionFactory sf = cfg.buildSessionFactory();
+		
+		Session sess = sf.openSession();
+		
+		Student ob = new Student(101, "Vikas", 23, 345.45f);
+		sess.save(ob);
+		
 		
 		SessionFactory sf = cfg.buildSessionFactory();
 		
